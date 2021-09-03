ce5ca9ad6e9274d5e5e2122819149a516fe76f7c
Gabriel França <89952261+gabrielbessa33@users.noreply.github.com>
2021-09-03 13:54:19 -0300

    Update imc.py

--- imc.py
+++ imc.py
@@ -1,20 +1,20 @@
-
+#Calculo para o IMC(Peso/Altura^2)
 peso = float(input("Informe seu peso: "))
 altura= float(input("Informe sua altura: "))
 
-def num_neg(): 
+def num_neg():  #Para saber se o número é negativo
     if peso or altura < 0:
         print("NUMERO INCORRETO")
         print("APENAS NUMEROS POSITIVOS PODEM SER INSERIDOS")
 
-def imc_c():
+def imc_c(): #O calculo
     imc = peso/(altura**2)
 
-def cal_imc(peso_):
+def cal_imc(peso_): #Mostrar o resultado
     imc = peso/(altura**2)
     print("Resultado do IMC é: ",round(imc, 2) , peso_)
 
-def imc_peso():
+def imc_peso(): #Mostrar a classificação do usuário
     if imc <= 18.5:
         cal_imc("ABAIXO DO PESO")
     elif imc > 18.6 and imc<= 24.9:
